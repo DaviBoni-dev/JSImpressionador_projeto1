575b815102f5aced60c7ed73503fc5d9e3111540
Davi Boni <bonidavi07@gmail.com>
2024-09-20 22:10:47 -0300

    atualizada as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o github");
 turboLogger("Estou aprendendo sobre GIT", "red");
+turboLogger("Eu consegui criar uma branche!", "darkgreen");
